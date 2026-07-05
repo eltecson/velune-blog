7c5fad8a307861c1e3a57aebbc9e3ca4de876c2e
Emmanuel Leu Tecson <emmanuel.l.tecson@gmail.com>
2026-07-05 20:04:30 +0800

    feat: Add image cards to landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,43 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { landingImages, landingImgSize } from "@/constants/images";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 export default function Landing() {
+  const router = useRouter();
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      
-    </div>
+    <main className="flex flex-col flex-1 items-center justify-center gap-[80px] font-display">
+      <div className="flex flex-col items-center px-[20px] gap-[20px]">
+        <h1 className="text-[24px] tracking-wider text-center">
+          Write Ideas That Last.
+        </h1>
+        <h2 className="text-base text-center leading-5">
+          Velune is a modern publishing platform for creators who want clean writing, thoughtful design, and a space that feels entirely their own.
+        </h2>
+      </div>
+      <Button
+        onClick={() => router.push("/register")}
+        className="px-[24px] py-[12px] bg-secondary rounded-full font-display text-base font-normal normal-case z-2"
+      >
+        Start Writing
+      </Button>
+      <div className="fixed">
+        {landingImages.map((img, index) => (
+          <Image
+            key={img.src}
+            src={img.src} alt={img.alt}
+            width={landingImgSize.mobile.width}
+            height={landingImgSize.mobile.height}
+            className={`
+              md:w-[${landingImgSize.desktop.width}px] h-auto rounded-[50px] fixed top-[55%] left-1/2
+              ${index === 0 ? "-translate-x-[120%] -rotate-25 translate-y-[2.5%]" : index === 1 ? "-translate-x-1/2 z-1" : "translate-x-[20%] rotate-25 translate-y-[2.5%]"}
+            `}
+            sizes={`(min-width: 768px) ${landingImgSize.desktop.height}px, ${landingImgSize.desktop.width}px`}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
---
+++ src/constants/images.ts
@@ -0,0 +1,26 @@
+const imageDir = "/images"
+export const landingImgSize = {
+  mobile: {
+    width: 270,
+    height: 540,
+  },
+  desktop: {
+    width: 500,
+    height: 1000,
+  },
+}
+
+export const landingImages = [
+  {
+    src: `${imageDir}/img3.png`,
+    alt: "",
+  },
+  {
+    src: `${imageDir}/img1.png`,
+    alt: "",
+  },
+  {
+    src: `${imageDir}/img2.png`,
+    alt: "",
+  },
+]
