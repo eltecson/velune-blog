2e0c5a7f9846efbc827318a4f507291b2bda358a
Emmanuel Leu Tecson <emmanuel.l.tecson@gmail.com>
2026-07-06 22:26:24 +0800

    feat: Make desktop footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,20 +5,20 @@ import Link from "@/components/ui/link";
 export default function Footer() {
   return (
     <footer className="bg-dark">
-      <div className="py-[64px] flex justify-center items-center">
+      <div className="py-[64px] md:py-[96px] flex justify-center items-center">
         <Link href="/">
-          <WideLogo className="w-auto h-[72px] text-complementary" />
+          <WideLogo className="w-auto h-[72px] md:h-[108px] text-complementary" />
         </Link>
       </div>
-      <div className="bg-complementary rounded-t-[32px] pt-[48px] pb-[32px] px-[20px] flex flex-col gap-[32px]">
-        <div className="grid grid-cols-2 gap-y-[32px]">
+      <div className="bg-complementary rounded-t-[32px] md:rounded-t-[64px] pt-[48px] md:pt-[96px] pb-[32px] md:pb-[64px] px-[20px] md:px-[80px] flex flex-col gap-[32px] md:gap-[64px]">
+        <div className="grid grid-cols-2 gap-y-[32px] md:flex md:justify-between">
           {footerNavigation.map(section => (
-            <div key={section.sectionTitle} className="flex flex-col gap-[8px] font-sans">
-              <h2 className="text-base font-bold tracking-wider leading-5">{section.sectionTitle}</h2>
-              <ul className="flex flex-col">
+            <div key={section.sectionTitle} className="flex flex-col gap-[8px] md:gap-[12px] font-sans">
+              <h2 className="text-base md:text-lg font-bold tracking-wider leading-5">{section.sectionTitle}</h2>
+              <ul className="flex flex-col md:gap-[4px]">
                 {section.sectionLinks.map(link => (
                   <li key={link.href}>
-                    <Link href={link.href} className="text-base tracking-wider leading-5 active:underline">
+                    <Link href={link.href} className="md:text-lg tracking-wider leading-5 active:underline hover:underline underline-offset-2">
                       {link.title}
                     </Link>
                   </li>
@@ -27,7 +27,7 @@ export default function Footer() {
             </div>
           ))}
         </div>
-        <p className="text-base tracking-wider self-center">© Velune 2026</p>
+        <p className="text-base md:text-lg tracking-wider self-center">© Velune 2026</p>
       </div>
     </footer>
   )
