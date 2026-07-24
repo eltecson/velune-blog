d8d35686c52d42752f0d03bb86682aa4d169435f
Emmanuel Leu Tecson <emmanuel.l.tecson@gmail.com>
2026-07-24 12:51:19 +0800

    feat: Add confirm close dialog

--- src/components/AccountDropdown/AccountDropdown.tsx
+++ src/components/AccountDropdown/AccountDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,22 +9,33 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { RiArrowDropDownFill } from "@remixicon/react";
-import { AccountDropdownProps } from "@/types/components";
+import { RiArrowDropDownFill } from "@remixicon/react"
+import { AccountDropdownProps } from "@/types/components"
 import Image from "next/image"
-import { cn } from "@/lib/utils";
-import { toast } from "sonner";
-import { MouseEvent } from "react";
-import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils"
+import { toast } from "sonner"
+import { MouseEvent, useState } from "react"
+import { useRouter } from "next/navigation"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog"
+import { Button } from "../ui/button"
 
 export default function AccountDropdown({
   className,
-  account
+  account,
 }: AccountDropdownProps) {
   const router = useRouter()
+  const [logoutDialogOpen, setLogoutDialogOpen] = useState(false)
 
-  const handleLogout = async (e: MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
+  const handleLogout = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
 
     try {
       const res = await fetch("/api/auth/logout", {
@@ -42,6 +55,8 @@ export default function AccountDropdown({
         position: "bottom-center",
       })
 
+      setLogoutDialogOpen(false)
+
       router.replace("/login")
       router.refresh()
     } catch (error) {
@@ -54,37 +69,84 @@ export default function AccountDropdown({
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className={cn(
-        `text-foreground flex flex-col items-center justify-center px-[4px] md:px-[20px]`,
-        className
-      )}>
-        <Image
-          width="32"
-          height="32"
-          src={account.avatarSrc} alt="Profile image"
-          className="w-[32px] h-[32px] rounded-full"
-          loading="eager"
-        />
-        <div className="hidden md:flex items-center">
-          <p className="text-[12px] font-display">
-            Me
-          </p>
-          <RiArrowDropDownFill className="size-4" />
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuGroup>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Write a new post</DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem onClick={handleLogout}>Log out</DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          className={cn(
+            "text-foreground flex flex-col items-center justify-center px-[4px] md:px-[20px]",
+            className
+          )}
+        >
+          <Image
+            width={32}
+            height={32}
+            src={account.avatarSrc}
+            alt="Profile image"
+            className="size-[32px] rounded-full"
+            loading="eager"
+          />
+
+          <div className="hidden md:flex items-center">
+            <p className="text-[12px] font-display">
+              Me
+            </p>
+            <RiArrowDropDownFill className="size-4" />
+          </div>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Write a new post</DropdownMenuItem>
+          </DropdownMenuGroup>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault()
+                setLogoutDialogOpen(true)
+              }}
+            >
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Dialog
+        open={logoutDialogOpen}
+        onOpenChange={setLogoutDialogOpen}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              Confirm logout
+            </DialogTitle>
+
+            <DialogDescription>
+              Are you sure you want to log out?
+              You’ll need to sign in again to access your account.
+            </DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter>
+            <Button onClick={handleLogout} className="bg-good text-complementary">
+              Yes
+            </Button>
+
+            <DialogClose asChild>
+              <Button className="bg-bad text-complementary">
+                No
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -41,9 +41,11 @@ function DropdownMenuContent({
     <DropdownMenuPrimitive.Portal>
       <DropdownMenuPrimitive.Content
         data-slot="dropdown-menu-content"
-        sideOffset={sideOffset}
         align={align}
-        className={cn("z-50 max-h-(--radix-dropdown-menu-content-available-height) w-(--radix-dropdown-menu-trigger-width) min-w-48 origin-(--radix-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-none bg-popover p-1.5 text-popover-foreground shadow-md ring-1 ring-foreground/10 duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:overflow-hidden data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95", className )}
+        alignOffset={-20}
+        sideOffset={8}
+        collisionPadding={20}
+        className={cn("z-50 max-h-(--radix-dropdown-menu-content-available-height) w-(--radix-dropdown-menu-trigger-width) min-w-48 origin-(--radix-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-[8px] ring-1 ring-foreground/50 bg-background p-1.5 text-foreground duration-100 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:overflow-hidden data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95", className )}
         {...props}
       />
     </DropdownMenuPrimitive.Portal>
@@ -73,7 +75,7 @@ function DropdownMenuItem({
       data-inset={inset}
       data-variant={variant}
       className={cn(
-        "group/dropdown-menu-item relative flex cursor-default items-center gap-2.5 rounded-none px-3 py-2 text-xs font-medium tracking-wider uppercase outline-hidden select-none focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-inset:pl-9.5 data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5 data-[variant=destructive]:*:[svg]:text-destructive",
+        "group/dropdown-menu-item font-display relative flex cursor-default items-center gap-2.5 rounded-[4px] cursor-pointer px-3 py-2 text-sm font-medium outline-hidden select-none focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-inset:pl-9.5 data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5 data-[variant=destructive]:*:[svg]:text-destructive",
         className
       )}
       {...props}
@@ -170,7 +172,7 @@ function DropdownMenuLabel({
       data-slot="dropdown-menu-label"
       data-inset={inset}
       className={cn(
-        "px-3 py-2 text-xs font-semibold tracking-wider text-muted-foreground uppercase data-inset:pl-9.5",
+        "px-3 py-2 text-xs tracking-wider text-foreground/80 data-inset:pl-9.5",
         className
       )}
       {...props}
