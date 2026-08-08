967abaa2b8a88899f18bf35a4ea9eed2296c7cc0
Emmanuel Leu Tecson <emmanuel.l.tecson@gmail.com>
2026-08-08 15:59:41 +0800

    feat: Add skeleton loader for Featured Post

--- src/components/posts/FeaturedPost.tsx
+++ src/components/posts/FeaturedPost.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import Link from "../ui/link";
+import { Skeleton } from "../ui/skeleton";
 
 export default function FeaturedPost({
   className,
@@ -37,43 +38,48 @@ export default function FeaturedPost({
     getFeaturedPost()
   }, [])
   return (
-    <div
-      className={cn(
-        "bg-primary border-b-1 border-foreground/50 w-full flex flex-col gap-[8px]",
-        "sm:px-[80px] sm:flex-row sm:gap-[20px]",
-        className
+    <>
+      {featuredPost ? (
+        <article
+          className={cn(
+            "bg-primary border-b-1 border-foreground/50 w-full flex flex-col gap-[8px]",
+            "md:pl-[44px] md:pr-[20px] lg:px-[80px] md:flex-row md:gap-[8px]",
+            className
+          )}
+          {...props}
+        >
+          <h2 className="sr-only">
+            Featured Post
+          </h2>
+          {featuredPost && (
+            <>
+              <Link href={"/posts/" + featuredPost.slug} className="flex-1">
+                <Image
+                  src={featuredPost.cover_image} alt="Featured post cover image"
+                  width={630}
+                  height={400}
+                  className="w-full h-auto md:h-full md:w-full"
+                />
+              </Link>
+              <div className={cn(
+                "text-complementary flex flex-1 flex-col gap-[8px] px-[20px] pt-[16px] pb-[32px]",
+                "md:justify-center"
+              )}>
+                <Link href={"/posts/" + featuredPost.slug}>
+                  <h3 className="text-2xl lg:text-3xl font-bold leading-tight active:underline hover:underline underline-offset-4">
+                    {featuredPost.title}
+                  </h3>
+                </Link>
+                <p className="text-base leading-tight lg:text-xl">
+                  {featuredPost.excerpt}
+                </p>
+              </div>
+            </>
+          )}
+        </article>
+      ): (
+        <Skeleton className="h-[450px] w-full md:h-[350px]" />
       )}
-      {...props}
-    >
-      <h2 className="sr-only">
-        Posts
-      </h2>
-      {featuredPost && (
-        <>
-          <Link href={"/posts/" + featuredPost.slug} className="flex-1">
-            <Image
-              src={featuredPost.cover_image} alt="Featured post cover image"
-              width={630}
-              height={400}
-              className="w-full h-auto"
-            />
-          </Link>
-          <div className={cn(
-            "text-complementary flex flex-1 flex-col gap-[8px] px-[20px] pt-[16px] pb-[32px]",
-            "sm:justify-center"
-          )}>
-            <Link href={"/posts/" + featuredPost.slug}>
-              <h3 className="text-2xl sm:text-3xl font-bold leading-tight active:underline hover:underline underline-offset-4">
-                {featuredPost.title}
-              </h3>
-            </Link>
-            <p className="text-base leading-tight sm:text-xl">
-              {featuredPost.excerpt}
-            </p>
-          </div>
-        </>
-      )}
-
-    </div>
+    </>
   )
 }
